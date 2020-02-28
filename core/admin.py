c77ac895679395b9b5df32a5deb2545e914f0d6a
Rodrigo Macedo <ramcorreio@yahoo.com.br>
2020-02-28 01:13:37 -0300

    atualizando aula 54

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cargo, Servico, Funcionario
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'ativo', 'modificado')
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'icone', 'ativo', 'modificado')
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'ativo', 'modificado')
